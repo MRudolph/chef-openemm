c22a6654a3fd1b18b0166d1f24cfb33282bec534
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-02-28 16:52:14 +0100

    inital release

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             'openemm'
+maintainer       'Maximilian Rudolph'
+license          'All rights reserved'
+description      'Installs/Configures openemm'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
